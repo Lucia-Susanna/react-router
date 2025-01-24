fd356fe8d36107fd1e258aff124bea816c336c8d
Lucia-Susanna <lucia_susanna@hotmail.it>
2025-01-24 12:20:40 +0100

    feat: stampa elenco ricette

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,8 +1,42 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 const Blog = () => {
+  const apiUrl = "http://localhost:3000"
+  const [postsList, setPostsList] = useState(null)
+
+  const fetchData = () => {
+    axios.get(`${apiUrl}/posts`)
+      .then(res => {
+        setPostsList(res.data)
+      })
+      .catch(err => {
+        console.error(`Errore nel caricamento: ${err}`);
+      })
+  }
+
+  console.log(postsList);
+
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <section className="container">
-      BLOG
+      <h1>Ricette salvate:</h1>
+      <ul>
+        {postsList?.map(post => (
+
+          <li className="d-flex justify-between">
+            <span className="titolo">{post.title}</span>
+            <span>
+              <a href="#">mostra dettagli</a>
+              <button><i className="fa-solid fa-trash-can"></i></button>
+            </span>
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
